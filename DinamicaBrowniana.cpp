b6cfce42532ac0f80c07945390fe34dbd64d6fb3
andreav13 <andreaaa21@hotmail.com>
2017-10-03 09:00:34 -0500

    creo que sirve pero me perdí hace horas

--- DinamicaBrowniana.cpp
+++ DinamicaBrowniana.cpp
@@ -8,25 +8,28 @@ using namespace std;
 const double Deltat=0.01, L=100, T=300, MASA= 22.8916, e=1, D=1.132, kb=0.826;//unidades: picosegundos, Amstromg, Kelvin, u.m.a.
 const double Gamma=kb*T/(MASA*D), sigma=sqrt(2*D*Deltat), dtU2mGamma=Deltat/(2*MASA*Gamma);
 
+const int N=4000;
+
 class Cuerpo;
 
 //Clase Cuerpo
 class Cuerpo{
 private:
-  double x,Fx,Fxold,m,R;
+  double x,xold,Fx,Fxold,m,R;
   
 public:
   void Inicie(double x0, double m0, double R0);
   void CalculeFuerza(double E);
   void Dibujese(void);
   void Muevase(Crandom & ran64);
-  double Getx(void){return x;}; 
+  double Getx(void){return x;};
+  double Getj(void){return (x-xold)/Deltat*e;};
 };
 
 
 //Funciones de la clase cuerpo
 void Cuerpo::Inicie(double x0, double m0, double R0){
-  x=x0; m=m0; R=R0; Fx=0;
+  x=xold=x0; m=m0; R=R0; Fx=0;
 }
 
 
@@ -36,6 +39,7 @@ void Cuerpo::CalculeFuerza(double E){
 
 
 void Cuerpo::Muevase(Crandom & ran64){
+  xold=x;
   x+=dtU2mGamma*(3*Fx-Fxold) + ran64.gauss(0,sigma); 
 }
 
@@ -47,7 +51,7 @@ void Cuerpo::Dibujese(void){
 //Funciones globales
 void InicieAnimacion(void){
   cout<<"set terminal gif animate"<<endl;
-  cout<<"set output 'DinBrowniana.gif'"<<endl;
+  cout<<"set output 'DinBrownianaMuchos.gif'"<<endl;
   cout<<"unset key"<<endl;
   cout<<"set xrange[-10:110]"<<endl;
   cout<<"set yrange[-10:10]"<<endl;
@@ -65,35 +69,62 @@ void TermineCuadro(void){
   cout<<endl;
 }
 
+double GetSigma2(Cuerpo * Na){
+  double xprom, sigma2;
+  int i;
+  
+  //calculo xprom
+  for(xprom=0,i=0;i<N;i++){
+    xprom+=Na[i].Getx();
+  }  xprom/=N;
+  
+  //calculo sigma
+  for(sigma2=0,i=0;i<N;i++){
+    sigma2+=pow(Na[i].Getx()-xprom,2);
+  }sigma2/=(N-1);
+  
+  return sigma2;
+
+}
+
+double Getjprom(Cuerpo * Na){
+  double jprom; int i;
+  for(jprom=0,i=0;i<N;i++){
+    jprom+=Na[i].Getj();
+  }jprom/=N;
+  return jprom;
+}
+
 
 //Main
 int main(void){
 
-  double tdibujo;  int Ndibujos=1000;
   double t,  tmax=40;
   Crandom ran64(1);
-  double E=0;
-  Cuerpo Na;//Sodio
+  double E=200, sigmax0=5;
+  Cuerpo Na[N];   //Sodio
+  int i;
 
-  InicieAnimacion();
+  
+  //InicieAnimacion();
   
   //(x0, m0, R0)
-  Na.Inicie(L/2, MASA, 4);
-  Na.CalculeFuerza(E);
+  for(i=0;i<N;i++){Na[i].Inicie(ran64.gauss(L/2, sigmax0), MASA, 4);}
+  for(i=0;i<N;i++){Na[i].CalculeFuerza(E);}
 
-  for(t=0, tdibujo=0;t<tmax;t+=Deltat, tdibujo+=Deltat){
-    if(tdibujo>tmax/Ndibujos){
-      InicieCuadro();
-      Na.Dibujese();
-      TermineCuadro();
-      tdibujo=0;
-
-      Na.CalculeFuerza(E);
-      Na.Muevase(ran64);
-      }
-
-    
+  for(t=0;t<tmax;t+=Deltat){
+    cout<<t<<" "<<Getjprom(Na)<<endl;
+      
+    //InicieCuadro();
+    //Na[0].Dibujese();
+    //TermineCuadro();
+     
+    for(i=0;i<N;i++){Na[i].CalculeFuerza(E);}
+    for(i=0;i<N;i++){Na[i].Muevase(ran64);}
   }
+
+    //IMPRIMA
+    //for(i=0;i<N;i++){cout<<Na[i].Getx()<<endl;}
   
   return 0;
 
